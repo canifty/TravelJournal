d1cbfb2237c699a1dacf23d71406e5de7ecbb7e8
Silvia Esposito <silviaesposito0215@gmail.com>
2024-10-15 11:57:01 +0200

    Modified buttons on the FileEditPage

--- TravelJournal/FileEditPage.swift
+++ TravelJournal/FileEditPage.swift
@@ -1,84 +1,153 @@
-//
-//  PencilButton.swift
-//  TravelJournal
-//
-//  Created by Silvia Esposito on 14/10/24.
-//
-
 import SwiftUI
+import UIKit
+
 struct FileEditPage: View {
-        @State private var isFavorite = false // Stato per il cuore
-
-        var body: some View {
-            VStack(alignment: .leading, spacing: 16) {
-                // Riga di pulsanti con icone
-                HStack(spacing: 50) {
-                    // Pulsante cuore
-                    Button(action: {
-                        isFavorite.toggle()
-                    }) {
-                        Image(systemName: isFavorite ? "heart.fill" : "heart")
-                            .font(.system(size: 24))
-                            .foregroundColor(isFavorite ? .red : .primary)
-                    }
-
-                    // Pulsante suono
-                    Button(action: {
-                        print("Suono premuto")
-                    }) {
-                        Image(systemName: "waveform")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-
-                    // Pulsante immagine
-                    Button(action: {
-                        print("Immagine premuta")
-                    }) {
-                        Image(systemName: "photo.on.rectangle")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-
-                    // Pulsante fotocamera
-                    Button(action: {
-                        print("Fotocamera premuta")
-                    }) {
-                        Image(systemName: "camera")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-
-                    // Pulsante matita
-                    Button(action: {
-                        print("Modifica premuta")
-                    }) {
-                        Image(systemName: "pencil")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
+    @State private var userInput = ""
+    @State private var isShowingCamera = false
+    @State private var isShowingPhotoLibrary = false
+    @State private var selectedImages: [UIImage] = [] // Array di immagini
+    @State private var isFavorite = false
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button("Cancel") {
+                    presentationMode.wrappedValue.dismiss()
                 }
-                .padding(.top)
-
-                // Titolo
-                Text("Naples")
-                    .font(.largeTitle)
-                    .bold()
-                    .padding(.horizontal)
-
-                // Descrizione
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies dapibus congue. Duis lacinia vulputate ligula, vitae volutpat orci tincidunt et.")
-                    .padding(.horizontal)
-
+                .padding(.leading)
+                
                 Spacer()
+                
+                Button("Done") {
+                    saveData()
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .padding(.trailing)
             }
-            .padding()
+            .padding(.top)
+            
+            Text("Naples")
+                .font(.largeTitle)
+                .bold()
+                .padding(.vertical)
+            
+            TextEditor(text: $userInput)
+                .frame(height: 150)
+                .padding()
+            
+            // Mostra tutte le immagini selezionate
+            if !selectedImages.isEmpty {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(selectedImages, id: \.self) { image in
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(10)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            
+            Spacer()
+            
+            // Barra inferiore con pulsanti
+            HStack(spacing: 50) {
+                // Pulsante cuore
+                Button(action: {
+                    isFavorite.toggle() // Cambia lo stato del cuore
+                }) {
+                    Image(systemName: isFavorite ? "heart.fill" : "heart")
+                        .font(.system(size: 24))
+                        .foregroundColor(isFavorite ? .red : .primary)
+                }
+                Button(action: {
+                        print("Suono premuto")
+                                    })
+                { Image(systemName: "waveform")
+                .font(.system(size: 24))
+                .foregroundColor(.primary)
+                                    }
+                                    
+                
+                Button(action: {
+                    isShowingPhotoLibrary = true
+                }) {
+                    Image(systemName: "photo.on.rectangle")
+                        .font(.system(size: 24))
+                        .foregroundColor(.primary)
+                }
+                .sheet(isPresented: $isShowingPhotoLibrary) {
+                    ImagePicker(sourceType: .photoLibrary, selectedImages: $selectedImages)
+                }
+                
+                // Pulsante fotocamera
+                Button(action: {
+                    isShowingCamera = true
+                }) {
+                    Image(systemName: "camera")
+                        .font(.system(size: 24))
+                        .foregroundColor(.primary)
+                }
+                .sheet(isPresented: $isShowingCamera) {
+                    ImagePicker(sourceType: .camera, selectedImages: $selectedImages)
+                }
+            }
+            .padding(.bottom, 40)
         }
+        .padding()
     }
-
-    struct JournalEdit_Previews: PreviewProvider {
+    
+    // Modifica ImagePicker per supportare array di immagini
+    struct ImagePicker: UIViewControllerRepresentable {
+        var sourceType: UIImagePickerController.SourceType
+        @Binding var selectedImages: [UIImage] // Cambiato per supportare un array di immagini
+        
+        class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+            let parent: ImagePicker
+            
+            init(parent: ImagePicker) {
+                self.parent = parent
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                if let image = info[.originalImage] as? UIImage {
+                    parent.selectedImages.append(image) // Aggiungi l'immagine all'array
+                }
+                picker.dismiss(animated: true)
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true)
+            }
+        }
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator(parent: self)
+        }
+        
+        func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = context.coordinator
+            return picker
+        }
+        
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    }
+    
+    func saveData() {
+        // Logica per salvare i dati
+        print("Dati salvati: \(userInput)")
+        print("Immagini selezionate: \(selectedImages.count)")
+    }
+    
+    struct FileEditPage_Previews: PreviewProvider {
         static var previews: some View {
             FileEditPage()
         }
     }
-
+}
